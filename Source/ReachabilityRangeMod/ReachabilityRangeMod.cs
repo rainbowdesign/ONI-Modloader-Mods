2b0bbf2a0ea9e719938aad3a2dbb22e9d722c3cc
javisar <javisar@gmail.com>
2018-09-18 14:06:52 +0200

    More test

--- Source/ReachabilityRangeMod/ReachabilityRangeMod.cs
+++ Source/ReachabilityRangeMod/ReachabilityRangeMod.cs
@@ -6,15 +6,58 @@ using Harmony;
 
 namespace ReachabilityRangeMod
 {
+	[HarmonyPatch(typeof(OffsetGroups), "BuildReachabilityTable")]
+	internal class ReachabilityRangeMod_OffsetGroups_BuildReachabilityTable
+	{
+
+		private static void Prefix(CellOffset[] area_offsets, CellOffset[][] table, CellOffset[] filter)
+		{
+			Debug.Log(" === ReachabilityRangeMod_OffsetGroups_BuildReachabilityTable Prefix === ");
+			Debug.Log("area_offsets: ");
+			foreach (CellOffset a in area_offsets)
+			{
+				Debug.Log(a.ToString());
+			}
+
+			Debug.Log("table: ");
+			foreach (CellOffset[] a in table)
+			{
+				foreach (CellOffset b in a)
+				{
+					Debug.Log(b.ToString());
+				}
+			}
+
+			Debug.Log("filter: ");
+			if (filter !=  null) 
+				foreach (CellOffset a in filter)
+				{
+					Debug.Log(a.ToString());
+				}
+		}
+
+		private static void Postfix(CellOffset[][] __result, CellOffset[] area_offsets, CellOffset[][] table, CellOffset[] filter)
+		{
+			Debug.Log(" === ReachabilityRangeMod_OffsetGroups_BuildReachabilityTable Postfix === ");
+			foreach (CellOffset[] a in __result)
+			{
+				foreach (CellOffset b in a)
+				{
+					Debug.Log(b.ToString());
+				}
+			}
+		}
+	}
+
 	[HarmonyPatch(typeof(Global),"Awake")]
-	internal class ReachabilityRangeMod_OffsetGroups_ctor
+	internal class ReachabilityRangeMod_Global_ctor
 	{
 
 		private static void Postfix()
 		{
-			Debug.Log(" === ReachabilityRangeMod_OffsetGroups_ctor Postfix === ");
+			Debug.Log(" === ReachabilityRangeMod_Global_ctor Postfix === ");
 			//OffsetGroups.InvertedStandardTable = InvertedStandardTable_Alt;
-			OffsetGroups.InvertedStandardTableWithCorners = InvertedStandardTableWithCorners_Alt;
+			//OffsetGroups.InvertedStandardTableWithCorners = InvertedStandardTableWithCorners_Alt;
 		}
 		/*
 		public static CellOffset[][] InvertedStandardTable_Alt = OffsetTable.Mirror(new CellOffset[28][]
