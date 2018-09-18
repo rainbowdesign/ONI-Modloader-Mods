8a12ab0a3f880a4bee95ae5c0b72b3e50ddc0110
javisar <javisar@gmail.com>
2018-09-18 11:47:05 +0200

    Tests NeuralVacillator

---
+++ Source/UnlimitedNeuralVacillatorMod/UnlimitedNeuralVacillatorMod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using static GeneShuffler;
+using static StateMachine;
+
+namespace UnlimitedNeuralVacillatorMod
+{
+	[HarmonyPatch(typeof(GeneShufflerSM), "InitializeStates")]
+	internal class UnlimitedNeuralVacillatorMod_GeneShufflerSM_InitializeStates
+	{
+
+		private static void Postfix(GeneShufflerSM __instance, BaseState default_state)
+		{
+			if (!UnlimitedNeuralVacillatorState.StateManager.State.Enabled) return;
+
+			Debug.Log(" === UnlimitedNeuralVacillatorMod_GeneShufflerSM_OnSpawn Postfix === ");
+
+			__instance.working.pst.EventTransition(GameHashes.AnimQueueComplete, __instance.idle);
+
+		}
+	}
+}
---
+++ Source/UnlimitedNeuralVacillatorMod/UnlimitedNeuralVacillatorState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ONI_Common.Json;
+
+namespace UnlimitedNeuralVacillatorMod
+{
+	public class UnlimitedNeuralVacillatorState
+	{
+		public bool Enabled { get; set; } = true;
+
+
+		public static BaseStateManager<UnlimitedNeuralVacillatorState> StateManager
+			= new BaseStateManager<UnlimitedNeuralVacillatorState>("Template");
+	}
+}
